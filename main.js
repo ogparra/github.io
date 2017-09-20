fbd457769f42c847d07843787d5777a34d1baf4e
ogparra <ogparra.15@gmail.com>
2017-09-20 16:23:13 -0700

    Website update

--- main.js
+++ main.js
@@ -1,34 +1,65 @@
 
+Vue.component('about-content', {
+  template: '<div>Hey my name is Oscar</div>'
+});
+
+
+Vue.component('experience-content', {
+  template: '<div> This is my experience</div>'
+});
+
+Vue.component('writing-content', {
+  template: '<div>Here is my current writing </div>'
+});
+
+Vue.component('portfolio-content',{
+  template: '<div> This is my current portfolio</div>'
+});
+
 /* For information on Vue check out https://vuejs.org */
 var app = new Vue({
   el: '#app',
   data: {
-  	main_content:true,
-  	sub_content: false,
-    about:false,
+  	main: true,
+    about: false,
+    experience: false,
+    writing: false,
     portfolio: false, 
-    exper: false, 
-    other: false, 
-    contact: false,
   },
     methods: {
-
-    toggle: function(value){
-      //Hide main content and show sub content
-    this.main_content = !this.main_content;
-    this.sub_content = !this.sub_content;
-
-    if(value == 1){
-			this.about = !this.about;
-		}else if(value == 2 ){
-			this.portfolio = !this.portfolio;
-		}else if(value == 3){
-			this.exper = !this.exper;
-		}else if(value == 4){
-			this.other = !this.other;
-		}else
-			value = value; // This case should never be reached
+      toggle: function(number){
+        if(number == 0){
+          this.main = true;
+          this.about = false; 
+          this.experience = false;
+          this.writing = false;
+          this.portfolio = false;
+        }else if(number == 1){
+          this.main = false;
+          this.about = true; 
+          this.experience = false;
+          this.writing = false;
+          this.portfolio = false;
+        }else if(number == 2){
+          this.main = false;
+          this.about = false; 
+          this.experience = true;
+          this.writing = false;
+          this.portfolio = false;
+        }else if(number == 3){
+          this.main = false;
+          this.about = false; 
+          this.experience = false;
+          this.writing = true;
+          this.portfolio = false;
+        }else{
+          this.main = false;
+          this.about = false; 
+          this.experience = false;
+          this.writing = false;
+          this.portfolio = true;
+        }
+      } 
     }
-  }
-})
+});
 
